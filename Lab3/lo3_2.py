6c2ffddad6b72c48c92af347f767f850f34f3d1b
marreann <marit@n181-p153.eduroam.kth.se>
2018-01-29 14:02:52 +0100

    steg 3 beta

--- Lab3/lo3_2.py
+++ Lab3/lo3_2.py
@@ -3,8 +3,9 @@
 from bintreefile import Bintree
 
 svenska = Bintree() #Skapa tomt träd med namn svenska
+engelska = Bintree()
 
-with open("word3.txt", "r") as svenskfil: #Öppna fil och döp till svenskfil
+'''with open("word3.txt", "r") as svenskfil: #Öppna fil och döp till svenskfil
     for rad in svenskfil: #För varje rad i svenskfil
         ordet = rad.strip() #Skiljer vi på ordet
         #  Ett trebokstavsord per rad
@@ -13,4 +14,21 @@ with open("word3.txt", "r") as svenskfil: #Öppna fil och döp till svenskfil
         else:
             svenska.addToTree(ordet)
 #svenska.printTree()# in i sökträdet
+print("\n")'''
+
+lista = []
+with open("engelska.txt", "r") as engelskafil: #Öppna fil och döp till svenskfil
+    for rad in engelskafil: #För varje rad i svenskfil
+        for ordet in rad.split():
+            lista += ordet
+            print ordet #Skiljer vi på ordet
+
+        #  Ett trebokstavsord per rad
+        #if ordet in engelska:
+         #   print(ordet)
+        #else:
+         #   engelska.addToTree(ordet)
+            #svenska.printTree()# in i sökträdet
+print lista
+#engelska.printTree()
 print("\n")
