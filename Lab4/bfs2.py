6921a7dfd1fd7b9f0744f0adc35c008f286339a1
annink <annink@kth.se>
2018-02-04 22:32:35 +0100

    Uppgift 2

--- Lab4/bfs2.py
+++ Lab4/bfs2.py
@@ -1,42 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import sys
+from LinkedQFile import *
+from bintreefile import *
 
-from bintreefile import Bintree
-
-svenska = Bintree() #Skapar tomt träd med namn svenska
+q = LinkedQ()
+svenska = Bintree()
 gamla = Bintree()
 
-#startord = str(input('Ange startord: ')) #Ska den finnas i ordlistan??
-#slutord = str(input('Ange slutord: '))
-
-# Skapar kombinationer av startordet utifrån alfabetet
-#TODO: När vi gör om ordet till lista kan det inte å,ä,ö avkodas. Kan göra om det på annat sätt?
 def makechildren(startord):
     alfabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','å','ä','ö']
-    #print startord
     startord = list(startord)
-    #print startord
 
     for i in range(len(startord)):
         for alfabet_letter in alfabet:
             if startord[i] == alfabet_letter:
                 pass
-
             else:
                 combination_startord=startord[:] #Så man kopierar lista
                 combination_startord[i] = alfabet_letter #Ersätter det index som vi kollar igenom med alfabet_letter
                 combination_startord=''.join(combination_startord) #Så man går från list till str
-                kollaord(combination_startord)
-
-# Kollar om ordet finns i den svenska ordlistan samt om bokstavskombinationen förekommit tidigare.
-def kollaord(ordet):
-    if ordet in svenska:
-        if ordet in gamla:
-            pass
-        else:
-            print (ordet)
-            gamla.addToTree(ordet)
+                if combination_startord in svenska and combination_startord not in gamla:
+                    q.enqueue(combination_startord)
+                    gamla.addToTree(combination_startord) #Sparar vi tillagda ord
 
 def test():
     # Läser in fil och skapar ett binärtträd från ordlistan.
@@ -44,12 +29,27 @@ def test():
         for rad in svenskfil:  # För varje rad i svenskfil
             ordet = rad.strip()
             if ordet in svenska:
-                pass #Ignorera dubletter i ordlistan
+                pass  # Ignorera dubletter i ordlistan
             else:
                 svenska.addToTree(ordet)
 
-    startord='söt'
-    makechildren(startord)
-    
+    startord = 'ute'
+    slutord = 'hit'
+    q.enqueue(startord)
+    found = False
+
+    while not q.isEmpty():
+        nod = q.dequeue()
+        if nod == slutord:
+            print ('Hittad!!')
+            found = True
+            print (nod)
+            break
+        else:
+            makechildren(nod)
+
+    if found == False:
+        print('Baajs den fanns inte')
 
 test()
+
