1acc36a549a1ffa7423073f3e643be0402d5bf5b
marann <marit@annink.com>
2018-02-19 10:17:51 +0100

    del2 klar

--- lab6/lab6.2.py
+++ lab6/lab6.2.py
@@ -1,8 +1,8 @@
 class Song:
-    def __init__(self, trackid, artisttime, artistname, tracklength, year):
+    def __init__(self, trackid, artistname, songtitle, tracklength, year):
         self.trackid = trackid
-        self.artisttime = artisttime
         self.artistname = artistname
+        self.songtitle = songtitle
         self.tracklength = tracklength
         self.year = year
 
@@ -12,7 +12,6 @@ def readfile(filename):
     with open(filename, "r") as tracks:
         for line in tracks:  # För varje rad i tracks
             song = line.strip().split('\t')
-            print (song)
             song = Song(song[0], song[1], song[2], song[3], song[4])  # Creates objects from file
             songlist.append(song)
 
@@ -20,21 +19,20 @@ def readfile(filename):
 
 # Function for linear searching. Check each element in list and compare.
 def linsearch(thelist,n):
-    maxlength = Song(0,0,0,0.0,0)
+    maxlength = thelist[0]
     songdict = {}
-    for i in range(0,n-1):
+    for i in range(0,n):
         for song in thelist:
             if song.tracklength > maxlength.tracklength and song not in songdict:  # Check if artist is found
                 maxlength = song
         songdict[i] = maxlength
     return songdict
 
-
 def main():
     filename = 'sang-artist-data.txt'
-
     thelist = readfile(filename)
-
-    #linsearch(thelist)
+    n = int(input('Vilket n för längsta låten?'))
+    songdict = linsearch(thelist,n)
+    print(songdict[n-1].songtitle)
 
 main()
