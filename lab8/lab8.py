5ba61001283721f9bfe526be6c354ae216349151
marann <marit@annink.com>
2018-03-01 18:46:34 +0100

    checkLetters

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -17,10 +17,24 @@ def checkMoleculeSyntax():
 
 #Function that checks if first letter is uppercase and if containing two that second is lowercase.
 def checkLetters(molecule):
-    length = 1
-    while length >= 2 and q.isEmpty():
+    length = 1 #Start at first letter
+    while length >= 2
         letter = q.peek()
+        
+        if letter.isupper and length == 1: #Check if uppercase, if true continue to next letter
+            length += 1
+            
+        elif length == 1:
+            raise GrammarError('No upper case: ' + letter)
+        
+        if letter.isalpha() and letter.islower(): #Check if next is alphabetical and lower case
+            length += 1
+            
+        elif length == 2:
+            rasie GrammarError('No lower case ' + letter)
 
+        return
+            
 #Function that checks if charachter is an int and bigger than 1.
 def checkNumbers():
     return True
