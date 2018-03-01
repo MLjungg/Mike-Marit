6b9e115d399a24a2f24ca68fdcf6ee218a066e02
marann <marit@annink.com>
2018-03-01 17:33:40 +0100

    start av 8

---
+++ lab8/lab8.py
@@ -0,0 +1,14 @@
+'''<molekyl> ::= <atom> | <atom><num>
+<atom>  ::= <LETTER> | <LETTER><letter>
+<LETTER>::= A | B | C | ... | Z
+<letter>::= a | b | c | ... | z
+<num>   ::= 2 | 3 | 4 | ...
+'''
+
+#Function that checks if first letter is uppercase and if containing two that second is lowercase.
+def readLetters():
+    return True
+
+#Function that checks if charachter is an int and bigger than 1.
+def readNumbers():
+    return True
