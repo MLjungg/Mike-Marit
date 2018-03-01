c94969423ad72c5c0f2d190214296091f803dc25
marann <marit@annink.com>
2018-03-01 18:17:01 +0100

    All functions

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -5,10 +5,34 @@
 <num>   ::= 2 | 3 | 4 | ...
 '''
 
+from LinkedQFile import *
+
+class GrammarError(Exception):
+    pass
+
+#Function for checking if molecule's syntax is correct
+def checkMoleculeSyntax():
+    checkLetters()
+    checkNumbers()
+
 #Function that checks if first letter is uppercase and if containing two that second is lowercase.
-def readLetters():
+def checkLetters():
     return True
 
 #Function that checks if charachter is an int and bigger than 1.
-def readNumbers():
+def checkNumbers():
     return True
+
+#Function that adds a molecule to list
+def addMolecule():
+    return True
+
+def checkSyntax():
+    return True
+
+def main():
+    return True
+
+main()
+
+
