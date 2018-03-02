114b9b9ff2f995008e3b379587e473d0d5f9e454
MLjungg <mikael.lj@hotmail.com>
2018-03-02 09:00:43 +0100

    Miner buggfixes

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -7,52 +7,57 @@
 
 from LinkedQFile import *
 
+
 class GrammarError(Exception):
     pass
 
-#Function for checking if molecule's syntax is correct
+
+# Function for checking if molecule's syntax is correct
 def checkMoleculeSyntax(molecule):
     checkLetters(molecule)
     checkNumbers(molecule)
 
-#Function that checks if first letter is uppercase and if containing two that second is lowercase.
+
+# Function that checks if first letter is uppercase and if containing two that second is lowercase.
 def checkLetters(molecule):
-    length = 1 #Start at first letter
+    length = 1  # Start at first letter
     letter = molecule.peek()
-    if letter.isupper and length == 1: #Check if uppercase, if true continue to next letter
+    if letter.isupper() and length == 1:  # Check if uppercase, if true continue to next letter
         molecule.dequeue()
         length += 1
-            
+
     elif length == 1:
-        raise GrammarError('Not upper case: ' + str(letter))
+        raise GrammarError('Not an alphabetic character or not upper case: ' + str(letter))
 
     letter = molecule.peek()
-    if letter.isalpha() and letter.islower() and length == 2: #Check if next is alphabetical and lower case
+    if letter.isalpha() and letter.islower() and length == 2:  # Check if next is alphabetical and lower case
         molecule.dequeue()
         length += 1
-            
+
     elif length == 2:
-        raise GrammarError('Not lower case ' + str(letter))
+        raise GrammarError('Not an alphabetic character or not lower case: ' + str(letter))
 
     return
-            
-#Function that checks if charachter is an int and bigger than 1.
+
+
+# Function that checks if charachter is an int and bigger than 1.
 def checkNumbers(molecule):
     while not molecule.isEmpty():
         charachter = molecule.peek()
-        try: #Check if it's an int and then execute check bigger than 1
-            number = int(charachter) 
+        try:  # Check if it's an int and then execute check bigger than 1
+            number = int(charachter)
             if number > 1:
                 molecule.dequeue()
             else:
                 raise GrammarError('Not an acceptable number: ' + str(charachter))
-        
-        except ValueError: #If not an int
+
+        except ValueError:  # If not an int
             raise GrammarError('Not an integer: ' + str(charachter))
 
     return
 
-#Function that adds a molecule to list
+
+# Function that adds a molecule to list
 def addMolecule(molecule):
     moleculeList = list(molecule)
     molecule = LinkedQ()
@@ -60,20 +65,23 @@ def addMolecule(molecule):
         molecule.enqueue(charachter)
     return molecule
 
+
 def checkSyntax(molecule):
     molecule = addMolecule(molecule)
     try:
         checkMoleculeSyntax(molecule)
         return 'Woop, woop, following the syntax'
-    
+
     except GrammarError as incorrect:
-        return str(incorrect)        
+        return str(incorrect)
+
 
 def main():
-    molecule = "Ch4"
+    molecule = "Ab-1"
     result = checkSyntax(molecule)
     print(result)
-    
+
+
 main()
 
-
+# If molecule = nothing, it crashes. Just a reminder if lab9 crashes in Kattis later on.
--- lab8/unittest1.py
+++ lab8/unittest1.py
@@ -8,21 +8,16 @@ class SyntaxTest(unittest.TestCase):
     def testCorrectAtom(self):
         # Testing a correct atom according to the rules.
         self.assertEqual(checkSyntax("Ch9"), 'Woop, woop, following the syntax')
+        self.assertEqual(checkSyntax("Xa99"), 'Woop, woop, following the syntax')
 
     def testNoncorrectAtomletter(self):
         # Testing a non-correct atomletter according to the rules.
-        self.assertEqual(checkSyntax("cr6"), 'Not upper case: c')
-
-    def testNoncorrectAtomletter(self):
-        # Testing a non-correct atomletter according to the rules.
-        self.assertEqual(checkSyntax("CH9"), 'Not lower case H')
+        self.assertEqual(checkSyntax("cr6"), 'Not an alphabetic character or not upper case: c')
+        self.assertEqual(checkSyntax("CH9"), 'Not an alphabetic character or not lower case: H')
 
     def testNoncorrectAtomnumber(self):
         # Testing a non-correct atomletter according to the rules.
-        self.assertEqual(checkSyntax("Cr0"), 'Too small integer: 0')
-
-    def testNoncorrectAtomnumber(self):
-        # Testing a non-correct atomletter according to the rules.
+        self.assertEqual(checkSyntax("Cr0"), 'Not an acceptable number: 0')
         self.assertEqual(checkSyntax("Cr-"), 'Not an integer: -')
 
 
