6ea7bc550b06d1a0bc7e5c206c7c2535339d0d84
marann <marit@annink.com>
2018-03-03 12:41:06 +0100

    Final version

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -10,16 +10,6 @@ import sys
 # <letter>::= a | b | c | ... | z
 # <num>   ::= 2 | 3 | 4 | ...
 
-'''Ditt program ska läsa formeln tecken för tecken och med rekursiv medåkning kolla syntaxen.
- Rekursiv medåkning innebär att huvudprogrammet först gör anropet readformel(),
- varefter readformel() anropar readmol() som anropar readgroup() och sedan eventuellt sej själv
- (men inte om inmatningen är slut eller om den just kommit tillbaka från ett parentesuttryck).
-
- Funktionen readgroup() anropar antingen readatom() eller läser en parentes och anropar readmol() etc - allt enligt grammatiken.
- När ett syntaxbrott upptäcks genereras en exception (raise Syntaxfel("Saknad högerparentes"))
- som fångas i huvudprogrammet och där skrivs hela resten av indataraden ut.'''
-
-#--------------------------------------------------------------------------------------------------
 
 class GrammarError(Exception):
     pass
@@ -27,7 +17,10 @@ class GrammarError(Exception):
 # Function for checking if molecule's syntax is correct
 def readformula(molecule):
     molecule = addMolecule(molecule)
-    readmol(molecule)
+    if not molecule.isEmpty():
+        readmol(molecule)
+    else:
+        raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule))
 
 def readmol(molecule):
     readgroup(molecule)
@@ -38,31 +31,36 @@ def readmol(molecule):
             raise GrammarError('Saknad högerparentes vid radslutet ' + str(molecule))
 
 def readgroup(molecule):
-    if molecule.peek() == '(':
-        global openlefts
-        openlefts += 1
-        molecule.dequeue()
-        readmol(molecule)
-    elif molecule.peek() == ')':
-        if openlefts > 0:
-            openlefts -= 1
+    if not molecule.isEmpty():
+        if molecule.peek() == '(':
+            global openlefts
+            openlefts += 1
             molecule.dequeue()
-            if molecule.peek().isnumeric():
-                readnumbers(molecule)
+            readmol(molecule)
+        elif molecule.peek() == ')':
+            if openlefts > 0:
+                openlefts -= 1
+                molecule.dequeue()
+                if not molecule.isEmpty() and molecule.peek().isnumeric():
+                    readnumbers(molecule)
+                else:
+                    raise GrammarError('Saknad siffra vid radslutet ' + str(molecule)) #+sista delen av molekylen
             else:
-                raise GrammarError('Saknad siffra vid radslutet ' + str(molecule)) #+sista delen av molekylen
+                raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule)) #+sista delen av molekylen
+                
+        elif molecule.peek().isalpha():
+            readatom(molecule)
+            if not molecule.isEmpty() and molecule.peek().isnumeric() :
+                readnumbers(molecule)
         else:
-            raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule)) #+sista delen av molekylen
-            
+            raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule))
     else:
-        readatom(molecule)
-        if (not molecule.isEmpty()) and molecule.peek().isnumeric() :
-            readnumbers(molecule)
+        raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule))
 
 # Function that checks if charachter is an int and bigger than 1.
 def readnumbers(molecule):
     firstIteration = 1
-    while (not molecule.isEmpty()) and molecule.peek().isnumeric():
+    while not molecule.isEmpty() and molecule.peek().isnumeric():
         number = int(molecule.dequeue())
         if firstIteration == 1:
             if number == 1:
@@ -101,12 +99,8 @@ def addMolecule(molecule):
     return molecule
 
 def readfile(file):
-    atomlist = []
-    with open(file,'r') as atomfile:
-        for line in atomfile:
-            atom = line.strip().split(' ')
-            atomlist = atom
-    return atomlist
+    line = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd   Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Fl Lv'
+    return line.strip().split(' ')
             
 def binarysort(atomlist):
     binaryatomlist = Bintree()
