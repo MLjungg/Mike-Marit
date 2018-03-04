ce6dff5da68050a8f8b55dee6e1453d652411c57
MLjungg <mikael.lj@hotmail.com>
2018-03-04 20:37:40 +0100

    added tests

---
+++ lab9/test1.py
@@ -0,0 +1,31 @@
+import unittest
+
+from lab9 import *
+
+class SyntaxTest(unittest.TestCase):
+
+    def testCorrectAtom1(self):
+        # Testing a correct atom according to the rules.
+        self.assertEqual(main("Na"), 'Formeln är syntaktiskt korrekt')
+
+    def testCorrectAtom2(self):
+        # Testing a non-correct atomletter according to the rules.
+        self.assertEqual(main("Si(C3(COOH)2)4(H2O)7"), 'Formeln är syntaktiskt korrekt')
+        self.assertEqual(main("Na332"), 'Formeln är syntaktiskt korrekt')
+
+if __name__ == '__main__':
+    unittest.main()
+
+    ''''
+    Sample input:
+    Na
+    H2O
+    Si(C3(COOH)2)4(H2O)7
+    Na332
+
+    Sample output:
+    Formeln är syntaktiskt korrekt
+    Formeln är syntaktiskt korrekt
+    Formeln är syntaktiskt korrekt
+    Formeln är syntaktiskt korrekt
+    '''
---
+++ lab9/test2.py
@@ -0,0 +1,76 @@
+import unittest
+
+from lab9 import *
+
+class SyntaxTest(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(main("C(Xx4)5"), 'Okänd atom vid radslutet 4)5')
+
+    def test2(self):
+        self.assertEqual(main("C(OH4)C"), 'Saknad siffra vid radslutet C')
+
+    def test3(self):
+        self.assertEqual(main("C(OH4C"), 'Saknad högerparentes vid radslutet ')
+
+    def test4(self):
+        self.assertEqual(main("H2O)Fe"), 'Felaktig gruppstart vid radslutet )Fe')
+
+    def test5(self):
+        self.assertEqual(main("H0"), 'För litet tal vid radslutet ')
+
+    def test6(self):
+        self.assertEqual(main("H1C"), 'För litet tal vid radslutet C')
+
+    def test7(self):
+        self.assertEqual(main("H02C"), 'För litet tal vid radslutet 2C')
+
+    def test8(self):
+        self.assertEqual(main("Nacl"), 'Saknad stor bokstav vid radslutet cl')
+
+    def test9(self):
+        self.assertEqual(main("a"), 'Saknad stor bokstav vid radslutet a')
+
+    def test10(self):
+        self.assertEqual(main("(Cl)2)3"), 'Felaktig gruppstart vid radslutet )3')
+
+    def test11(self):
+        self.assertEqual(main(")"), 'Felaktig gruppstart vid radslutet )')
+
+    def test12(self):
+        self.assertEqual(main("2"), 'Felaktig gruppstart vid radslutet 2')
+
+if __name__ == '__main__':
+    unittest.main()
+
+''''
+Sample input:
+C(Xx4)5
+C(OH4)C
+C(OH4C
+H2O)Fe
+H0
+H1C
+H02C
+Nacl
+a
+(Cl)2)3
+)
+2
+#
+
+
+Sample output:
+Okänd atom vid radslutet 4)5
+Saknad siffra vid radslutet C
+Saknad högerparentes vid radslutet
+Felaktig gruppstart vid radslutet )Fe
+För litet tal vid radslutet
+För litet tal vid radslutet C
+För litet tal vid radslutet 2C
+Saknad stor bokstav vid radslutet cl
+Saknad stor bokstav vid radslutet a
+Felaktig gruppstart vid radslutet )3
+Felaktig gruppstart vid radslutet )
+Felaktig gruppstart vid radslutet 2
+'''
