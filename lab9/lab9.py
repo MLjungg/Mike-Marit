3f58b0610961889560e7671f1fa0d85d88c7f273
marann <marit@annink.com>
2018-03-05 10:40:11 +0100

    comments

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -22,33 +22,35 @@ def readformula(molecule):
     else:
         raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule))
 
+#Check if our molecule is a molecule 
 def readmol(molecule):
     readgroup(molecule)
     if not molecule.isEmpty():
-        readmol(molecule)
+        readmol(molecule) #check if we have group+mol
     else:
         if openlefts > 0:
             raise GrammarError('Saknad högerparentes vid radslutet ' + str(molecule))
 
+#Check if molecule contains of a group
 def readgroup(molecule):
     if not molecule.isEmpty():
-        if molecule.peek() == '(':
+        if molecule.peek() == '(': #(<mol>)<num>
             global openlefts
             openlefts += 1
             molecule.dequeue()
             readmol(molecule)
-        elif molecule.peek() == ')':
+        elif molecule.peek() == ')': #(<mol>)<num>
             if openlefts > 0:
                 openlefts -= 1
                 molecule.dequeue()
                 if not molecule.isEmpty() and molecule.peek().isnumeric():
                     readnumbers(molecule)
                 else:
-                    raise GrammarError('Saknad siffra vid radslutet ' + str(molecule)) #+sista delen av molekylen
+                    raise GrammarError('Saknad siffra vid radslutet ' + str(molecule)) 
             else:
-                raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule)) #+sista delen av molekylen
+                raise GrammarError('Felaktig gruppstart vid radslutet ' + str(molecule)) 
                 
-        elif molecule.peek().isalpha():
+        elif molecule.peek().isalpha(): #<atom> | <atom><num>
             readatom(molecule)
             if not molecule.isEmpty() and molecule.peek().isnumeric() :
                 readnumbers(molecule)
@@ -67,19 +69,20 @@ def readnumbers(molecule):
                 if not molecule.isEmpty() and molecule.peek().isnumeric():
                     firstIteration += 1
                 else:
-                    raise GrammarError('För litet tal vid radslutet ' + str(molecule)) #Glöm ej str-func
+                    raise GrammarError('För litet tal vid radslutet ' + str(molecule)) 
             elif number > 1:
                 firstIteration += 1
             else:
-                raise GrammarError('För litet tal vid radslutet ' + str(molecule)) #Glöm ej str-func
+                raise GrammarError('För litet tal vid radslutet ' + str(molecule)) 
 
+#Check if molecule is a correct atom
 def readatom(molecule):
     firstletter = molecule.peek()
     if not firstletter.isupper():
-        raise GrammarError('Saknad stor bokstav vid radslutet ' + str(molecule)) #+radslut
+        raise GrammarError('Saknad stor bokstav vid radslutet ' + str(molecule))
     molecule.dequeue()
-    atom = firstletter
-    if not molecule.isEmpty():
+    atom = firstletter #Save for later check in atomlist
+    if not molecule.isEmpty(): 
         secondletter = molecule.peek()
         if secondletter.isalpha() and secondletter.islower():
             molecule.dequeue()
@@ -91,15 +94,14 @@ def readatom(molecule):
 
 # Function that adds a molecule to list
 def addMolecule(molecule):
-
     moleculeList = list(molecule)
     molecule = LinkedQ()
     for charachter in moleculeList:
         molecule.enqueue(charachter)
     return molecule
 
-def readfile(file):
-    line = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd   Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Fl Lv'
+def readfile():
+    line = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Fl Lv'
     return line.strip().split(' ')
             
 def binarysort(atomlist):
@@ -107,10 +109,11 @@ def binarysort(atomlist):
     for atom in atomlist:
         binaryatomlist.addToTree(atom)
     return binaryatomlist
-    
+
 def main(molecule):
-    filename = 'atoms.txt'
-    atomlist = readfile(filename)
+    global openlefts
+    openlefts = 0
+    atomlist = readfile()
     global binaryatomlist
     binaryatomlist = binarysort(atomlist)
 
@@ -119,26 +122,15 @@ def main(molecule):
         print('Formeln är syntaktiskt korrekt')
     except GrammarError as incorrect:
         print(str(incorrect))
-
-    
-
-    
-    
-    #atomLista= skapaAtomlista()
-    #hashtabell = lagraHashtabell(atomLista) #Fungerar hit. Det här kommer behövas för att undersöka om det är en verklig atom.
-    #addMolecule(molecule)
-
-    #result = checkSyntax(molecule)
-    #print(result)
-
+ 
 
 if __name__ == '__main__':
-	for row in sys.stdin:
-		row = row.strip()
-		if row != "#":
-                        openlefts = 0
-                        main(row)
-		else:
-			break
+    for row in sys.stdin:
+        row = row.strip()
+        if row != "#":
+            openlefts = 0
+            main(row)
+        else:
+            break
 
 
